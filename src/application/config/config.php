a5275ae3da6d1f19b70c1d77719956178d9eb758
bspbey <98183612+bspbey@users.noreply.github.com>
2022-06-06 19:42:05 +0300

    Update config.php

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Ultra-Film/';
+$config['base_url'] = 'http://demirciitiraf.unaux.com/';
 
 /*
 |--------------------------------------------------------------------------
